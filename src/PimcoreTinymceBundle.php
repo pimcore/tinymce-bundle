b1e05157d6c493ddd18ea9aa5da5da56062dd5df
Christian Fasching <fashxp@users.noreply.github.com>
2025-04-30 09:33:40 +0200

    License Switch to POCL (#1)
    * License Switch to POCL
    
    * License Switch to POCL

--- src/PimcoreTinymceBundle.php
+++ src/PimcoreTinymceBundle.php
@@ -2,13 +2,13 @@
 declare(strict_types=1);
 
 /**
- * Pimcore
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
  *
- * This source file is available under following license:
- * - Pimcore Commercial License (PCL)
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
  */
 
 namespace Pimcore\Bundle\TinymceBundle;
