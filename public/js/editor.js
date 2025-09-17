9969449aa545368b49aaaf7977d47fd738a29b44
Ji Jia Jia <kingjia90@gmail.com>
2025-09-17 19:04:00 +0200

    apply only on documents

--- public/js/editor.js
+++ public/js/editor.js
@@ -23,12 +23,16 @@ pimcore.bundle.tinymce.editor = Class.create({
     },
 
     initializeWysiwyg: function (e) {
+
         if (e.detail.context === 'object') {
             if (!isNaN(e.detail.config.maxCharacters) && e.detail.config.maxCharacters > 0) {
                 this.maxChars = e.detail.config.maxCharacters;
-            }else{
+            } else {
                 this.maxChars = -1;
             }
+            e.detail.config = Object.assign({}, e.detail.config, {ui_mode: 'combined'});
+        } else {
+            e.detail.config = Object.assign({}, e.detail.config, {ui_mode: 'split'});
         }
 
         this.config = e.detail.config;
@@ -114,7 +118,6 @@ pimcore.bundle.tinymce.editor = Class.create({
             ],
             content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
             inline: true,
-            ui_mode: 'split',
             base_url: '/bundles/pimcoretinymce/build/tinymce',
             suffix: '.min',
             convert_urls: false,
